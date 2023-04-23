fd705a2a23996aa598c2ea5123a65a00737a70bf
Ramlen97 <rameshwarlenka97@gmail.com>
2023-04-24 01:20:16 +0530

    task26

--- src/routes/todos.ts
+++ src/routes/todos.ts
@@ -1,31 +1,45 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-let todos = [];
-const router = (0, express_1.Router)();
+import { Router } from 'express';
+
+import { Todo } from '../models/todo';
+
+type RequestBody = { text: string };
+type RequestParams = { todoId: string };
+
+let todos: Todo[] = [];
+
+const router = Router();
+
 router.get('/', (req, res, next) => {
     res.status(200).json({ todos: todos });
-});
+})
+
 router.post('/todo', (req, res, next) => {
-    const newTodo = {
+    const body=req.body as RequestBody;
+    const newTodo: Todo = {
         id: new Date().toISOString(),
-        text: req.body.text
-    };
+        text: body.text
+    }
     todos.push(newTodo);
-    res.status(201).json({ message: 'Added new todo successfully', todo: newTodo, todos: todos });
-});
+    res.status(201).json({ message: 'Added new todo successfully', todo: newTodo, todos: todos })
+})
+
 router.post('/delete/:todoId', (req, res) => {
-    const id = req.params.todoId;
+    const params=req.params as RequestParams;
+    const id = params.todoId;
     todos = todos.filter(todo => todo.id !== id);
     res.status(200).json({ message: 'Deleted todo successfully', todos: todos });
-});
+})
+
 router.post('/update/:todoId', (req, res) => {
-    const id = req.params.todoId;
+    const body=req.body as RequestBody;
+    const params=req.params as RequestParams;
+    const id = params.todoId;
     const indx = todos.findIndex(todo => todo.id === id);
     if (indx >= 0) {
-        todos[indx].text = req.body.text;
+        todos[indx].text = body.text;
         return res.status(200).json({ message: 'Updated todo successfully', todo: todos[indx], todos: todos });
     }
     res.status(404).json({ message: 'Could not find todo for this id' });
-});
-exports.default = router;
+})
+
+export default router;
